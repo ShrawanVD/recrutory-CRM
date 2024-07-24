2bc13e4c1d594e6571cecef693e6cdc568efe72c
ShrawanVD <shra1.deo7714@gmail.com>
2024-07-24 15:25:07 +0530

    mastersheet api

--- src/app/services/leads/leads.service.ts
+++ src/app/services/leads/leads.service.ts
@@ -13,29 +13,29 @@ export class LeadsService {
 
   // mastersheet api 
   getAllLeads(){
-    return this.http.get('http://localhost:4000/api/master/candidates');
+    return this.http.get('https://recrutory-crm-backend.onrender.com/api/master/candidates');
   }
 
   getLeadById(id:any){
-    return this.http.get(`http://localhost:4000/api/master/candidates/${id}`);
+    return this.http.get(`https://recrutory-crm-backend.onrender.com/api/master/candidates/${id}`);
   }
 
   createLead(data:any){
-    return this.http.post("http://localhost:4000/api/master/candidates",data);
+    return this.http.post("https://recrutory-crm-backend.onrender.com/api/master/candidates",data);
   }
 
   deleteLeadById(id:any){
-    return this.http.delete(`http://localhost:4000/api/master/candidates/${id}`);
+    return this.http.delete(`https://recrutory-crm-backend.onrender.com/api/master/candidates/${id}`);
   }
 
   updateLeadById(id: any, data: any) {
-    return this.http.put(`http://localhost:4000/api/master/candidates/${id}`, data);
+    return this.http.put(`https://recrutory-crm-backend.onrender.com/api/master/candidates/${id}`, data);
   }
 
   langFilter(lang:any,proficiencyLevels: any){
     console.log("This is lang: ",lang)
     console.log("This is profi: ",proficiencyLevels)
-    return this.http.get(`http://localhost:4000/api/master/langfilter?lang=${lang}&proficiencyLevel=${proficiencyLevels}`);
+    return this.http.get(`https://recrutory-crm-backend.onrender.com/api/master/langfilter?lang=${lang}&proficiencyLevel=${proficiencyLevels}`);
   }
 
   // updating recruiter
